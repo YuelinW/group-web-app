6e581a53f045b09318f93c8cac1b8208e853f5ec
Heyang Chen <yuelin@iMac.local>
2022-11-24 00:02:33 -0800

    add explore page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router";
 import {BrowserRouter} from "react-router-dom";
 import RestaurantSummaryList from "./restaurant-summary-list/index.js";
 import HomeComponent from "./home";
+import ExploreComponent from "./explore";
 
 function App() {
   return (
@@ -13,8 +14,13 @@ function App() {
         <div className="col-2 col-md-2 col-lg-1 col-xl-2">
           <NavigationSidebar/>
         </div>
-        <div className="col-7 col-md-7 col-lg-8 col-xl-7">
-          <HomeComponent/>
+        <div className="col-7 col-md-7 col-lg-8 col-xl-7"
+             style={{"position": "relative"}}>
+          <Routes>
+            <Route index element={<HomeComponent/>}/>
+            <Route path="home"    element={<HomeComponent/>}/>
+            <Route path="explore" element={<ExploreComponent/>}/>
+          </Routes>
         </div>
       </div>
 
--- src/navigation-sidebar/index.js
+++ src/navigation-sidebar/index.js
@@ -1,25 +1,28 @@
 import React from "react";
-// import {Link} from "react-router-dom";
-// import {useLocation} from "react-router";
+import {Link} from "react-router-dom";
+import {useLocation} from "react-router";
 
 const NavigationSidebar = () => {
-  // const {pathname} = useLocation();
-  // const paths = pathname.split('/')
-  // const active = paths[1];
+  const {pathname} = useLocation();
+  const paths = pathname.split('/')
+  const active = paths[1];
   return (
       <>
         <div className="list-group">
           <img height={100} src="https://user-images.githubusercontent.com/113388766/203717920-a7b8176c-6402-436e-ae68-973ec3bd953e.png"/>
-          <a className="list-group-item">Home</a>
-          <a className="list-group-item">Explore</a>
-          <a className="list-group-item">Profile</a>
 
-          {/*<Link to="/home" className={`list-group-item ${active === 'home'?'active':''}`}>*/}
-          {/*  Home*/}
-          {/*</Link>*/}
-          {/*<Link to="/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>*/}
-          {/*  Explore*/}
-          {/*</Link>*/}
+          <Link to="/home" className={`list-group-item ${active === 'home'?'active':''}`}>
+            <i className="fa-solid fa-house"></i>
+            Home
+          </Link>
+          <Link to="/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
+            <i className="fa-solid fa-hashtag"></i>
+            Explore
+          </Link>
+          <Link to="/profile" className={`list-group-item ${active === 'profile'?'active':''}`}>
+            <i className="bi bi-person-fill"></i>
+            Profile
+          </Link>
         </div>
         <div className="d-grid mt-2">
           <a href="/login" className="btn btn-primary btn-block rounded-pill">
