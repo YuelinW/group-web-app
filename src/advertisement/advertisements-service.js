84a9be7b68a622f0898311dd53cbbe132d945eaa
Yuelin Wen <yuelin@iMac.local>
2022-12-06 13:12:52 -0800

    update services

--- src/advertisement/advertisements-service.js
+++ src/advertisement/advertisements-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
+const API_BASE = process.env.REACT_APP_API_BASE; // || 'http://localhost:4000/api';
 const YEALP_API = `${API_BASE}/advertisements`;
 
 export const findAllAdvertisements = async () => {
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({withCredentials: true})
-const BASE_URL = 'http://localhost:4000/api'
+const BASE_URL = process.env.REACT_APP_API_BASE; // || 'http://localhost:4000/api';
 
 export const findAllUsers = async () => {
   const response = await axios.get(`${BASE_URL}/users`)
